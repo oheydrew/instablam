7dc987fa24ee996966d39cdd53ebabd66241ef3e
oheydrew <drew@oheydrew.me>
2018-04-20 11:59:02 +1000

    Add user-test to check dotenv works

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'the first user account' do
+    before(:each) do
+      @user = User.new
+    end
+
+    it 'should take email from .env.test' do
+      @user.email = ENV.fetch('USER_EMAIL')
+      expect(@user.email).to eq('drew@oheydrew.me')
+    end
+  end
+
+  it 'should hash password' do
+    user = User.new
+  end
 end
