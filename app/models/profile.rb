5734cc5d10dc161d49fef04a29e6a464e37eb398
oheydrew <drew@oheydrew.me>
2018-04-21 16:45:53 +1000

    Fix profile model: full_name full_address

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,7 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   def full_address
-    "#{@profile.premise}, #{@profile.thoroughfare}, #{@profile.city}, #{@profile.administrative_area}, #{@profile.postal_code}, #{@profile.country_code}"
+    "#{premise}, #{thoroughfare}, #{city}, #{administrative_area}, #{postal_code}, #{country}"
   end
 
 end
