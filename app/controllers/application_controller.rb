023215fe8ef869d11b6e9191b8e6628b908a8bcd
oheydrew <drew@oheydrew.me>
2018-04-22 15:37:49 +1000

    Add pundit posts methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   # pundit authorization
   include Pundit
+
   protect_from_forgery with: :exception
+  
   # devise authentication
   before_action :authenticate_user!
 end
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,14 +1,25 @@
 class PostPolicy < ApplcationPolicy
 
   def index?
-    user.admin
-  end
-
-  def update?
-    user == record.user
+    true
   end
 
   def create?
-    user == record.user
+    user.present?
+  end
+
+  def update?
+    true if user.present? && user == post.user
+  end
+
+  def destroy?
+    true if user.present? && user == post.user
+  end
+
+  private
+
+  # just a nice little namespace method
+  def post
+    record
   end
 end
